0cc778967159cfeea0d685c0e848416ff5b64de5
Alicia <mxlgsq@126.com>
2019-12-27 10:19:24 +0800

    add demo1

---
+++ demo1/src/pages/Main.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="root">
+        test
+    </div>
+</template>
+<script>
+export default {
+  name: 'Main'
+}
+</script>
+<style rel="stylesheet/scss" lang="scss" scoped>
+</style>
